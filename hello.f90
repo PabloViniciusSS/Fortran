e29d5616bd57470e50586abc089f3ab22bc99e09
Pablo Vinícius Sousa Silva <48532322+PabloViniciusSS@users.noreply.github.com>
2023-10-19 09:05:03 -0300

    Initial commit

---
+++ hello.f90
@@ -0,0 +1,6 @@
+program hello !Nome do programação é obrigatorio ao iniciar
+implicit none !Utilizar o implicit none é uma boa prática de programação
+
+    
+print*, "Hello Word"
+end program  !obrigatorio o fechamento 
---
+++ operadores.f90
@@ -0,0 +1,20 @@
+program operadores
+implicit none
+
+!Declaração de variaveis
+
+integer:: a, b
+
+a = 1
+b=2
+
+write(*,*) "a+b", a+b
+write(*,*) "a-b", a-b
+write(*,*) "a*b", a*b
+write(*,*) "a/b", a/b
+write(*,*) "a**b",a**b
+
+
+
+
+end program operadores
